02d9bf305c5e9ba84d13d2432f46048876b6055f
Shiva Reddy <shivajayaramireddy@gmail.com>
2022-03-24 10:45:39 -0400

    added print table feature

--- app/static/script.js
+++ app/static/script.js
@@ -1,3 +1,4 @@
+// const { default: jsPDF } = require("jspdf");
 
 //! Modify button for each lot on homepage
 $modifyButton = $('.modify');
@@ -49,4 +50,63 @@ async function check_editor() {
 
 };
 check_editor();
+
+
+function generate() {
+  var doc = new jsPDF('p', 'pt', 'letter');
+  var htmlstring = '';
+  var tempVarToCheckPageHeight = 0;
+  var pageHeight = 0;
+  pageHeight = doc.internal.pageSize.height;
+  specialElementHandlers = {
+      // element with id of "bypass" - jQuery style selector  
+      '#bypassme': function (element, renderer) {
+          // true = "handled elsewhere, bypass text extraction"  
+          return true
+      }
+  };
+  margins = {
+      top: 150,
+      bottom: 60,
+      left: 40,
+      right: 40,
+      width: 600
+  };
+  var y = 20;
+  doc.setLineWidth(2);
+  doc.text(200, y = y + 30, "All Lots");
+  doc.autoTable({
+      html: '#main-lots-table',
+      startY: 70,
+      theme: 'grid',
+      // columnStyles: {
+      //     0: {
+      //         cellWidth: 180,
+      //     },
+      //     1: {
+      //         cellWidth: 180,
+      //     },
+      //     2: {
+      //         cellWidth: 180,
+      //     }
+      // },
+      // styles: {
+      //     minCellHeight: 40
+      // }
+  })
+  doc.save('All_lots.pdf');
+}
+
+//! Download table into PDF
+const $print_table = $('#print-table')
+$print_table.on('click', function (e){
+  e.preventDefault();
+
+  // download pdf
+  console.log("STARTING FUNCTION");
+  generate()
+
+
+
+});
 
