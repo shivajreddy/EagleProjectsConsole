53f7ac6d45b5ed02462e9efee63d6600fc41ddfb
Shiva Reddy <shivajayaramireddy@gmail.com>
2022-04-12 18:15:54 -0400

    hiding config, and pushing config variables to heroku

--- app/views/mail.py
+++ app/views/mail.py
@@ -16,7 +16,7 @@ def send_confirmation_mail(recipient_email, token):
   msg = Message(
     subject="Please confirm your registration to Eagle Console",
     recipients= [recipient_email, "shivajreddy@outlook.com", "sreddy@tecofva.com"],
-    sender= "consoleadmin@tecofva.com",
+    sender= "console@tecofva.com",
     # body = f"This is the link {link}",
     # html= f'<html lang="en"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Document</title></head><body> <a href="127.0.0.1.5000/confirm/email/token" target="_blank">Confirm Registration</a> </body></html>'
     html= """
