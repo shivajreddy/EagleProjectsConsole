12e7c03ec439c342d8bf512096184553c4330284
Shiva Reddy <shivajayaramireddy@gmail.com>
2022-04-21 18:36:06 -0400

    auto report part1 - test mail with APScheduler

--- app/features/autoreport.py
+++ app/features/autoreport.py
@@ -1,28 +1,46 @@
-
-from app.views import mail
-from flask_mail import Message 
+from app import app
+from flask_mail import Mail, Message
+mail = Mail(app)
 
 msg = Message(
   subject="Daily Report",
   recipients=["sreddy@tecofva.com"],
-  sender="",
-  html = ""
+  sender="shivatecofva@gmail.com",
+  html = "this is the message text"
 )
 
-
+# Get the lots and categorize them
 from app.Models.Lot import LotsDirectory
-from flask_apscheduler import APScheduler
 
+# Make the subject automatic -> xD-xE-xp
+
+
+all_lots_list = ['lot10', 'lot52', 'lot21']
+  # LotsDirectory.query.filter_by(id=1).first()
+  # lots = LotsDirectory.query.all()
+  # Drafting - OVERDUE
+  # Drafting - Due this week
+  # Engineering - OVERDUE
+  # Engineering - Due this week
+  # Plat - OVERDUE
+  # Plat - Due this week
+
+
+from flask_apscheduler import APScheduler
 scheduler = APScheduler()
 
 
 # msg.html = ""
-
+count = 0
 def job():
+  # if you don't start with app's context then when ever this function
+  # runs, it is going to start on another thread.
+  with app.app_context():
+    for lot in all_lots_list:
+      msg.html += lot
 
-  # LotsDirectory.query.filter_by(id=1).first()
-  lots = LotsDirectory.query.all()
+    mail.send(msg)
 
-
-  mail.send()
-  print("this is the jobbb")
+    global count
+    count += 1
+    print("job done", count)
--- run.py
+++ run.py
@@ -1,15 +1,17 @@
 """Application entry point"""
 from app import app
 
-from flask_apscheduler import APScheduler
-scheduler = APScheduler()
+# from flask_apscheduler import APScheduler
+# scheduler = APScheduler()
 
-def my_tasks_func():
-  print("this is the task")
+from app.features.autoreport import scheduler, job
+
+# def my_tasks_func():
+  # print("this is the task")
 
 if __name__ == "__main__":
 
-  scheduler.add_job(id='schedule task', func=my_tasks_func, trigger='interval', seconds=5)
+  scheduler.add_job(id='schedule task', func=job, trigger='interval', seconds=30)
   scheduler.start()
 
   app.run(host='0.0.0.0', port=8080, debug=True)
