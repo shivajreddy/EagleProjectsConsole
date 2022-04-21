c38eed7f0af326087c167098a8561c3c697f6ce3
Shiva Reddy <shivajayaramireddy@gmail.com>
2022-04-21 17:10:10 -0400

    auto report part1 - test mail with APScheduler

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,8 @@ app = Flask(__name__)
 
 
 #* Load Configurations
-app.config.from_object('config')
+# app.config.from_object('config')
+app.config.from_object('local_config')
 
 #* Connect Databaase
 from flask_sqlalchemy import SQLAlchemy
